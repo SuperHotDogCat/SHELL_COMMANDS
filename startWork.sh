10e364c94060e232b6ff2e6df2530b21614248e4
SuperHotDog <115141367+SuperHotDogCat@users.noreply.github.com>
2023-12-29 03:25:52 +0900

    Create startWork.sh

---
+++ startWork.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+open https://www.google.co.jp/ #macOSのみ, Linuxならxgd-open
+open https://discord.com #discordで連絡を取る人用
+open /Applications/LINE.app #LINEを使う人, OSによってpathは違うかも
+open /Applications/Slack.app #Slackを使う人, ブラウザの場合はhttpsを指定
+code . #VSCodeがopenで開けるようになっていること
