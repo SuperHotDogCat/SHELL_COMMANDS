5fed808687f9ab7107a9dc54dc07139840fd5ea4
SuperHotDog <115141367+SuperHotDogCat@users.noreply.github.com>
2024-01-01 02:20:45 +0900

    Update bgm.sh
    Specify from stdin which music to play first

--- bgm.sh
+++ bgm.sh
@@ -2,8 +2,14 @@
 
 # ディレクトリ内の音楽ファイルを取得
 music_files=("$1"/*.mp3)
+index=$2 
+if ! [[ $index =~ ^[0-9]+$ ]]; then
+    index=0 #標準入力がなかったら最初から再生する。
+fi
+echo $index
 # 音楽を再生
-for music_file in "${music_files[@]}"; do
+for ((i=index; i<${#music_files[@]}; i++)); do
+    music_file="${music_files[$i]}"
     play "$music_file" &
     player_pid=$!
     wait $player_pid
